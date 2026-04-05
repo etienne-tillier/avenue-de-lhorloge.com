06803c1f8641106798a75741cf6d315c5bed9f32
etienne-tillier <etienne.tillier@etu.umontpellier.fr>
2026-04-05 11:37:38 +0200

    🐛 fix: add robust legacy URL catch-all redirects

--- app/collections/horloge-coucou/page.tsx
+++ app/collections/horloge-coucou/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function HorloCoucouRedirect() {
-  redirect("/blog/horloge-coucou");
+  redirect("/blog/collections-horloge-coucou");
 }
